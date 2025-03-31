181dfabb3fc6881f9584e0901807ef0ad2d674a8
whitneyjdc <whitneyjdc@gmail.com>
2025-03-31 09:22:23 -0400

    Update fileecho.py

--- fileecho.py
+++ fileecho.py
@@ -1,3 +1,9 @@
 #Read in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
 
+curFile = open("echo.txt")
+for curLine in curFile:
+    print(curLine)
+curFile.close()
+
+#print(curLine)
