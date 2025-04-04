6ecc2c6ce14e2fbaaec5d40ca315e8431b5161a5
whitneyjdc <whitneyjdc@gmail.com>
2025-04-04 19:02:33 -0400

    Update highlowaverage.py
    I got it to read in the list of numbers, but couldn't get it to do much more.

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,20 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+f = open("numbers.txt", "r")
+print(f.read())
+
+for _ in range("numbers.txt"):
+    sum = 0
+    count = 0
+    average = 0
+    highest = 0
+    lowest = 0
+
+    count = len(set("numbers.txt"))
+
+print(f"Sum of the list:", sum)
+print(f"Number of numbers in the file is: ", len("numbers.txt"))
+
+f.close()
